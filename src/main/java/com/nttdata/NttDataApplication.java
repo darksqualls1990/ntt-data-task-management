cdb9c099709b095c288c010b5d9f30c391dc3912
hiramirezc <hiramirezc@indracompany.com>
2022-05-21 10:50:23 -0500

    Se sube creacion del proyecto

---
+++ src/main/java/com/nttdata/NttDataApplication.java
@@ -0,0 +1,19 @@
+package com.nttdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NttDataApplication {
+	
+	/**
+	 * Main method to start the spring boot application.
+	 * 
+	 * @param args application  arguments.
+	 */
+	public static void main(String[] args)  
+	{    
+		SpringApplication.run(NttDataApplication.class, args);    
+	}   
+
+}
