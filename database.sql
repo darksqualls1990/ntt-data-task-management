0c8351b84c45f67e232ffe927f245898dee4949e
darksqualls1990 <infrahector1@gmail.com>
2022-06-07 19:54:42 -0500

    Se suben comentarios

--- database.sql
+++ database.sql
@@ -1,13 +1,16 @@
-
+---Database
 CREATE DATABASE acennture WITH OWNER postgres;
+Commit;
 
+--Secuencia
 create sequence sec_accenture_shared_folder
   start with 1
   increment by 1
   maxvalue 99999
   minvalue 1;
-  
-  
+Commit;
+
+--Tabla 
 create table acc_shared_folder(
   id_shared_folder_ INTEGER NOT NULL DEFAULT NEXTVAL('sec_accenture_shared_folder'),
   id_album INTEGER,
@@ -17,3 +20,17 @@ create table acc_shared_folder(
   primary key (id_shared_folder_)
  );  
  
+
+ Commit;
+
+
+COMMENT ON COLUMN acc_shared_folder.id_shared_folder_ IS 'Identificador del album compartido';
+COMMENT ON COLUMN acc_shared_folder.id_album IS 'Identificador del album';
+COMMENT ON COLUMN acc_shared_folder.id_usser IS 'Identificador del usuario';
+COMMENT ON COLUMN acc_shared_folder.per_read IS 'Permiso de lectura - true es que que tiene permiso  y false es que no';
+COMMENT ON COLUMN acc_shared_folder.per_write IS 'Permiso de escritura - true es que que tiene permiso  y false es que no';
+
+
+COMMENT ON TABLE acc_shared_folder IS 'Tabla que almacena los permisos a los albumnes compartidos por usuario';
+
+Commit;
